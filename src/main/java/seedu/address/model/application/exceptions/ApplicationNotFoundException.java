881b75325586baf364a03e99e21e8850e33daa38
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-07 16:44:29 +0800

    Exception classes of "Application" class: JavaDoc updated.
    Following the repurposing of "Person" class to "Application" class,
    the JavaDoc of these class has become irrelevant.
    
    JavaDoc inaccuracies need to be addressed so that developers reading
    our code can have a better understanding.
    
    Let's update the JavaDoc of both exception classes.
    
    Going through and updating the JavaDoc of these classes will
    suffice.

--- src/main/java/seedu/address/model/application/exceptions/ApplicationNotFoundException.java
+++ src/main/java/seedu/address/model/application/exceptions/ApplicationNotFoundException.java
@@ -1,6 +1,6 @@
 package seedu.address.model.application.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified person.
+ * Signals that the operation is unable to find the specified application.
  */
 public class ApplicationNotFoundException extends RuntimeException {}
