8d393fbd7ee7a2bb8a760b6932c4d77720a2ade4
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-14 18:19:54 +0800

    "MainWindow" class: updated methods.
    The class is still using naming conventions from AB3 codebase.
    
    The method names used in this class is irrelevant in the context of
    InternApply.
    
    Let's update the methods found in "MainWindow".
    
    A simple refactoring will suffice since we are still going to reuse
    the method implementation from AB3 in InternApply.
    
    InternApply is still able to successfully run all previously
    implemented tests (Test passed:251 of 251 tests).

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -163,7 +163,7 @@ public class MainWindow extends UiPart<Stage> {
         primaryStage.hide();
     }
 
-    public ApplicationListPanel getPersonListPanel() {
+    public ApplicationListPanel getApplicationListPanel() {
         return applicationListPanel;
     }
 
