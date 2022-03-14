84f1a10ee815e50fe920ba33503318f37ce32a1c
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-14 20:33:34 +0800

    "UserPrefs" class: updated the field "addressBookFilePath".
    Currently, "addressBookFilePath" specifies the name of the file the
    app should search for on the user's local hard disk to load existing
    data.
    
    The file name as well as the name of the field is no longer relevant
    in the context of InternApply.
    
    Let's rename "addressBookFilePath" to "internApplyMemoryFilePath"
    and also update the name of the json file from "addressbook.json" to
    "internapplymemory.json".
    
    These changes are reflected on Lines 17, 51, 56, 71 and 76.
    
    InternApply now fails 2 of the 251 implemented tests
    (Test passed:249 of 251 tests).
    
    They are:
    1. readUserPrefs_fileInOrder_successfullyRead()
    2. readUserPrefs_extraValuesInFile_extraValuesIgnored()
    
    This will need to be addressed in a later commit.

--- src/main/java/seedu/address/model/UserPrefs.java
+++ src/main/java/seedu/address/model/UserPrefs.java
@@ -14,7 +14,7 @@ import seedu.address.commons.core.GuiSettings;
 public class UserPrefs implements ReadOnlyUserPrefs {
 
     private GuiSettings guiSettings = new GuiSettings();
-    private Path addressBookFilePath = Paths.get("data" , "addressbook.json");
+    private Path internApplyMemoryFilePath = Paths.get("data" , "internapplymemory.json");
 
     /**
      * Creates a {@code UserPrefs} with default values.
@@ -48,12 +48,12 @@ public class UserPrefs implements ReadOnlyUserPrefs {
     }
 
     public Path getInternApplyMemoryFilePath() {
-        return addressBookFilePath;
+        return internApplyMemoryFilePath;
     }
 
     public void setInternApplyMemoryFilePath(Path internApplyMemoryFilePath) {
         requireNonNull(internApplyMemoryFilePath);
-        this.addressBookFilePath = internApplyMemoryFilePath;
+        this.internApplyMemoryFilePath = internApplyMemoryFilePath;
     }
 
     @Override
@@ -68,19 +68,19 @@ public class UserPrefs implements ReadOnlyUserPrefs {
         UserPrefs o = (UserPrefs) other;
 
         return guiSettings.equals(o.guiSettings)
-                && addressBookFilePath.equals(o.addressBookFilePath);
+                && internApplyMemoryFilePath.equals(o.internApplyMemoryFilePath);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(guiSettings, addressBookFilePath);
+        return Objects.hash(guiSettings, internApplyMemoryFilePath);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Gui Settings : " + guiSettings);
-        sb.append("\nLocal data file location : " + addressBookFilePath);
+        sb.append("\nLocal data file location : " + internApplyMemoryFilePath);
         return sb.toString();
     }
 
