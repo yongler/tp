cd1708f81dd789391da11d984ae151eec691e363
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-16 23:30:03 +0800

    "JsonSerializableInternApplyTest" JUnit class: update naming convention.
    "JsonSerializableInternApplyTest" is a JUnit class from the AB3 code
    base.
    
    Since we are morphing AB3 to InternApply to track Internship
    applications, this test should reflect the change from person to
    application.
    
    Let's update the naming conventions used in this JUnit test.
    
    InternApply is able to successfully run all tests in this JUnit
    class.
    
    Folder "JsonSerializableAddressBookTest" renamed to
    "JsonSerializableInternApplyTest".
    
    Json files contained within the folder has also been renamed.

--- src/test/java/seedu/address/storage/JsonSerializableInternApplyTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableInternApplyTest.java
@@ -16,29 +16,29 @@ import seedu.address.testutil.TypicalApplications;
 public class JsonSerializableInternApplyTest {
 
     private static final Path TEST_DATA_FOLDER =
-            Paths.get("src", "test", "data", "JsonSerializableAddressBookTest");
-    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAddressBook.json");
-    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAddressBook.json");
-    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
+            Paths.get("src", "test", "data", "JsonSerializableInternApplyMemoryTest");
+    private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalApplicationInternApplyMemory.json");
+    private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidApplicationInternApplyMemory.json");
+    private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicateApplicationInternApplyMemory.json");
 
     @Test
-    public void toModelType_typicalPersonsFile_success() throws Exception {
+    public void toModelType_typicalApplicationsFile_success() throws Exception {
         JsonSerializableInternApply dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
                 JsonSerializableInternApply.class).get();
-        InternApplyMemory addressBookFromFile = dataFromFile.toModelType();
-        InternApplyMemory typicalPersonsAddressBook = TypicalApplications.getTypicalInternApplyMemory();
-        assertEquals(addressBookFromFile, typicalPersonsAddressBook);
+        InternApplyMemory internApplyMemoryFromFile = dataFromFile.toModelType();
+        InternApplyMemory typicalApplicationInternApplyMemory = TypicalApplications.getTypicalInternApplyMemory();
+        assertEquals(internApplyMemoryFromFile, typicalApplicationInternApplyMemory);
     }
 
     @Test
-    public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
+    public void toModelType_invalidApplicationFile_throwsIllegalValueException() throws Exception {
         JsonSerializableInternApply dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
                 JsonSerializableInternApply.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
     @Test
-    public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
+    public void toModelType_duplicateApplications_throwsIllegalValueException() throws Exception {
         JsonSerializableInternApply dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
                 JsonSerializableInternApply.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableInternApply.MESSAGE_DUPLICATE_PERSON,
