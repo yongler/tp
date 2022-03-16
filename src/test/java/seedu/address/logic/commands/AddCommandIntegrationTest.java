2d801840d25cea75972019703d3c4047373a9c31
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-16 21:27:25 +0800

    "AddCommandIntegrationTest" JUnit class: update naming convention.
    "AddCommandIntegrationTest" is a JUnit class from the AB3 code base.
    
    Since we are morphing AB3 to InternApply to track Internship
    applications, this test should reflect the change from person to
    application.
    
    Let's update the naming conventions used in this JUnit test.
    
    InternApply is able to successfully run all tests in this JUnit
    class.

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -26,7 +26,7 @@ public class AddCommandIntegrationTest {
     }
 
     @Test
-    public void execute_newPerson_success() {
+    public void execute_newApplication_success() {
         Application validApplication = new ApplicationBuilder().build();
 
         Model expectedModel = new ModelManager(model.getInternApplyMemory(), new UserPrefs());
@@ -37,7 +37,7 @@ public class AddCommandIntegrationTest {
     }
 
     @Test
-    public void execute_duplicatePerson_throwsCommandException() {
+    public void execute_duplicateApplication_throwsCommandException() {
         Application applicationInList = model.getInternApplyMemory().getApplicationList().get(0);
         assertCommandFailure(new AddCommand(applicationInList), model, AddCommand.MESSAGE_DUPLICATE_APPLICATION);
     }
