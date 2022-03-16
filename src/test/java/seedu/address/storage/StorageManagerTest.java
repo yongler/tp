1ecf7ac47d51eb989c32c5830e19123accbc2c86
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-16 23:37:25 +0800

    "StorageManagerTest" JUnit class: update naming convention.
    "StorageManagerTest" is a JUnit class from the AB3 code base.
    
    Since we are morphing AB3 to InternApply to track Internship
    applications, this test should reflect the change from person to
    application.
    
    Let's update the naming conventions used in this JUnit test.
    
    InternApply is able to successfully run all tests in this JUnit
    class.

--- src/test/java/seedu/address/storage/StorageManagerTest.java
+++ src/test/java/seedu/address/storage/StorageManagerTest.java
@@ -24,9 +24,9 @@ public class StorageManagerTest {
 
     @BeforeEach
     public void setUp() {
-        JsonInternApplyStorage addressBookStorage = new JsonInternApplyStorage(getTempFilePath("ab"));
+        JsonInternApplyStorage internApplyStorage = new JsonInternApplyStorage(getTempFilePath("ab"));
         JsonUserPrefsStorage userPrefsStorage = new JsonUserPrefsStorage(getTempFilePath("prefs"));
-        storageManager = new StorageManager(addressBookStorage, userPrefsStorage);
+        storageManager = new StorageManager(internApplyStorage, userPrefsStorage);
     }
 
     private Path getTempFilePath(String fileName) {
@@ -48,11 +48,11 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void addressBookReadSave() throws Exception {
+    public void internApplyMemoryReadSave() throws Exception {
         /*
          * Note: This is an integration test that verifies the StorageManager is properly wired to the
-         * {@link JsonAddressBookStorage} class.
-         * More extensive testing of UserPref saving/reading is done in {@link JsonAddressBookStorageTest} class.
+         * {@link JsonInternApplyStorage} class.
+         * More extensive testing of UserPref saving/reading is done in {@link JsonInternApplyStorageTest} class.
          */
         InternApplyMemory original = getTypicalInternApplyMemory();
         storageManager.saveInternApply(original);
@@ -61,7 +61,7 @@ public class StorageManagerTest {
     }
 
     @Test
-    public void getAddressBookFilePath() {
+    public void getInternApplyMemoryFilePath() {
         assertNotNull(storageManager.getInternApplyFilePath());
     }
 
