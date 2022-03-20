bdb16a0c15e0622564e86c2dbb1251a904c3df9a
Tan Yu Tao <tanyutao913@gmail.com>
2022-03-20 17:46:44 +0800

    Update Json serializer to support new Detail fields Update CliSyntax for edit command to support new Detail field Update SampleData to include default Details field

--- src/main/java/seedu/address/model/application/Details.java
+++ src/main/java/seedu/address/model/application/Details.java
@@ -5,9 +5,9 @@ import static java.util.Objects.requireNonNull;
 public class Details {
 
     //TODO finalize the default detail format
-    private static final String DEFAULT_DETAIL = "To add details, use the edit command";
+    public static final String DEFAULT_DETAIL = "To add details, use the edit command";
 
-    private final String detail;
+    public final String detail;
 
     /**
      * Constructs a {@code Details}.
@@ -24,5 +24,12 @@ public class Details {
     public Details(String detailInput) {
         requireNonNull(detailInput);
         detail = detailInput;
+    }
+
+    /**
+     * Returns true if a given string is a placeholder for an empty detail.
+     */
+    public static boolean isNotSet(String test) {
+        return test.equals(DEFAULT_DETAIL);
     }
 }
