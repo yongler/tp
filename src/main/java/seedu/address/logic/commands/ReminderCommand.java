7e94098a39035cf7619bf164d888a15e33c27796
lchokhoe <chokhoeefps335f@gmail.com>
2022-03-26 09:48:34 +0800

    Update "ReminderCommand".
    Currently "ReminderCommand" has still not been fully implemented.
    
    As such, users who use the command should be informed that this
    command is still being implemented.
    
    Let's create a placeholder message that gets given as a response
    when users input this command.
    
    We create a MESSAGE_NOT_IMPLEMENTED_YET String that gets used to
    create the CommandResult when ReminderCommand#execute() is called.
    
    InternApply is still able to successfully run all previously
    implemented tests (Test passed:264 of 264 tests).
    
    Also create a MESSAGE_USAGE String for consistency. All command
    classes have this message by default.

--- src/main/java/seedu/address/logic/commands/ReminderCommand.java
+++ src/main/java/seedu/address/logic/commands/ReminderCommand.java
@@ -7,8 +7,17 @@ public class ReminderCommand extends Command{
 
     public final static String COMMAND_WORD = "reminder";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Reminds you of your upcoming interviews."
+            + "If you have no upcoming intervi  ews a response "
+            + "informing you as such will be given instead.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
+            "Remark command not implemented yet";
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return new CommandResult("This is a reminder");
+        return new CommandResult(MESSAGE_NOT_IMPLEMENTED_YET);
     }
 }
