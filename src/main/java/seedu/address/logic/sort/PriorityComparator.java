0c6be878e54bc56c946b297768a7292788619bcc
Zheng Teck <zhengteck.goh@comp.nus.edu.sg>
2022-03-29 15:45:59 +0800

    Update Priority Comparator
    Fixed priority comparator comparing wrong values.

--- src/main/java/seedu/address/logic/sort/PriorityComparator.java
+++ src/main/java/seedu/address/logic/sort/PriorityComparator.java
@@ -4,17 +4,20 @@ import java.util.Comparator;
 import java.util.Optional;
 
 import seedu.address.model.application.Application;
-import seedu.address.model.tag.PriorityTag;
 import seedu.address.model.tag.PriorityTagType;
+import seedu.address.model.tag.Tag;
 
 public class PriorityComparator implements Comparator<Application> {
 
     public static final String COMMAND_WORD = "priority";
 
+    private static final String PRIORITY_HIGH = "[" + PriorityTagType.HIGH + "]";
+    private static final String PRIORITY_LOW = "[" + PriorityTagType.LOW + "]";
+
     @Override
     public int compare(Application o1, Application o2) {
-        Optional<PriorityTag> p1 = o1.getPriorityTag();
-        Optional<PriorityTag> p2 = o2.getPriorityTag();
+        Optional<Tag> p1 = o1.getPriorityTag();
+        Optional<Tag> p2 = o2.getPriorityTag();
 
         if (p1.isPresent() && p2.isEmpty()) {
             return 1;
@@ -24,12 +27,12 @@ public class PriorityComparator implements Comparator<Application> {
             return o1.getName().toString().compareTo(o2.getName().toString());
         } else {
             if (p1.get().toString().equals(p2.get().toString())) {
-                return 0;
-            } else if (p1.get().toString().equals(PriorityTagType.HIGH.toString())) {
+                return o1.getName().toString().compareTo(o2.getName().toString());
+            } else if (p1.get().toString().equals(PRIORITY_HIGH)) {
                 return 1;
-            } else if (p2.get().toString().equals(PriorityTagType.HIGH.toString())) {
+            } else if (p2.get().toString().equals(PRIORITY_HIGH)) {
                 return -1;
-            } else if (p2.get().toString().equals(PriorityTagType.LOW.toString())) {
+            } else if (p2.get().toString().equals(PRIORITY_LOW)) {
                 return 1;
             } else {
                 return -1;
