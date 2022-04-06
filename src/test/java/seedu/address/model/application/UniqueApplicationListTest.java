0a038c168b35d3f4e95b7434a232fd28c40a79b4
Zheng Teck <zhengteck.goh@comp.nus.edu.sg>
2022-04-07 01:31:43 +0800

    Add test cases for sorting applications

--- src/test/java/seedu/address/model/application/UniqueApplicationListTest.java
+++ src/test/java/seedu/address/model/application/UniqueApplicationListTest.java
@@ -15,6 +15,11 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.sort.ApplicationStatusComparator;
+import seedu.address.logic.sort.InterviewSlotComparator;
+import seedu.address.logic.sort.NameComparator;
+import seedu.address.logic.sort.PriorityComparator;
 import seedu.address.model.application.exceptions.ApplicationNotFoundException;
 import seedu.address.model.application.exceptions.DuplicateApplicationException;
 import seedu.address.testutil.ApplicationBuilder;
@@ -171,4 +176,172 @@ public class UniqueApplicationListTest {
         assertThrows(UnsupportedOperationException.class, ()
             -> uniqueApplicationList.asUnmodifiableObservableList().remove(0));
     }
+
+    @Test
+    public void sortApplication_nullPointerException() {
+        assertThrows(NullPointerException.class, () -> uniqueApplicationList.sort(new NameComparator(), null));
+        assertThrows(NullPointerException.class, () -> uniqueApplicationList.sort(null,
+                ListCommand.COMMAND_ORDER_WORD_ASCENDING));
+    }
+
+    @Test
+    public void sortApplication_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> uniqueApplicationList.sort(new NameComparator(),
+                "abc"));
+    }
+
+    @Test
+    public void sortApplication_checkSorting_name() {
+        UniqueApplicationList uniqueApplicationList = new UniqueApplicationList();
+        uniqueApplicationList.add(GRAB);
+        uniqueApplicationList.add(GARENA);
+
+        uniqueApplicationList.sort(new NameComparator(), ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(GARENA.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        uniqueApplicationList.sort(new NameComparator(), ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(GRAB.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+    }
+
+    @Test
+    public void sortApplication_checkSorting_interviewSlot() {
+        final Application application1 = new ApplicationBuilder()
+                .withName("Application A").withInterviewSlot("07-04-2022 12:45").build();
+        final Application application2 = new ApplicationBuilder()
+                .withName("Application B").withInterviewSlot("07-04-2022 13:45").build();
+        final Application application3 = new ApplicationBuilder()
+                .withName("Application C").withInterviewSlot("07-04-2022 13:45").build();
+        final Application application4 = new ApplicationBuilder()
+                .withName("Application D").withInterviewSlot("07-04-2022 13:45").build();
+
+        final InterviewSlotComparator interviewSlotComparator = new InterviewSlotComparator();
+
+        final UniqueApplicationList uniqueApplicationList = new UniqueApplicationList();
+        uniqueApplicationList.add(application1);
+        uniqueApplicationList.add(application2);
+        uniqueApplicationList.add(application3);
+
+        // Check interview slot
+        uniqueApplicationList.sort(interviewSlotComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application1.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        uniqueApplicationList.sort(interviewSlotComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application3.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        // Check wildcard ordering - order by name
+        uniqueApplicationList.sort(interviewSlotComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application2.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(1).getName());
+        assertEquals(application3.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(2).getName());
+
+        // Applications with no date
+        uniqueApplicationList.add(application4);
+
+        uniqueApplicationList.sort(interviewSlotComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application1.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        uniqueApplicationList.sort(interviewSlotComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application4.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+    }
+
+    @Test
+    public void sortApplication_checkSorting_priority() {
+        final Application application1 = new ApplicationBuilder()
+                .withName("Application A").withTags("HIGH").build();
+        final Application application2 = new ApplicationBuilder()
+                .withName("Application B").withTags("MEDIUM").build();
+        final Application application3 = new ApplicationBuilder()
+                .withName("Application C").withTags("LOW").build();
+        final Application application4 = new ApplicationBuilder()
+                .withName("Application D").withTags("LOW").build();
+        final Application application5 = new ApplicationBuilder()
+                .withName("Application E").build();
+
+        final PriorityComparator priorityComparator = new PriorityComparator();
+
+        final UniqueApplicationList uniqueApplicationList = new UniqueApplicationList();
+        uniqueApplicationList.add(application1);
+        uniqueApplicationList.add(application2);
+        uniqueApplicationList.add(application3);
+
+        // Check priority sort
+        uniqueApplicationList.sort(priorityComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application3.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        uniqueApplicationList.sort(priorityComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application1.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        // Check wildcard ordering - order by name
+        uniqueApplicationList.add(application4);
+        uniqueApplicationList.sort(priorityComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application4.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(1).getName());
+        assertEquals(application3.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        // Applications without priority
+        uniqueApplicationList.add(application5);
+        uniqueApplicationList.sort(priorityComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application5.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        uniqueApplicationList.sort(priorityComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application1.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+    }
+
+    public void sortApplication_checkSorting_status() {
+        final Application application1 = new ApplicationBuilder()
+                .withName("Application A").withTags("NOT_APPLIED").build();
+        final Application application2 = new ApplicationBuilder()
+                .withName("Application B").withTags("APPLIED").build();
+        final Application application3 = new ApplicationBuilder()
+                .withName("Application C").withTags("INTERVIEWED").build();
+        final Application application4 = new ApplicationBuilder()
+                .withName("Application D").withTags("REJECTED").build();
+        final Application application5 = new ApplicationBuilder()
+                .withName("Application E").withTags("ACCEPTED").build();
+        final Application application6 = new ApplicationBuilder()
+                .withName("Application F").withTags("ACCEPTED").build();
+        final Application application7 = new ApplicationBuilder()
+                .withName("Application G").build();
+
+        final ApplicationStatusComparator applicationStatusComparator = new ApplicationStatusComparator();
+
+        final UniqueApplicationList uniqueApplicationList = new UniqueApplicationList();
+        uniqueApplicationList.add(application1);
+        uniqueApplicationList.add(application2);
+        uniqueApplicationList.add(application3);
+        uniqueApplicationList.add(application4);
+        uniqueApplicationList.add(application5);
+
+        // Check sorting
+        uniqueApplicationList.sort(applicationStatusComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application5.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+        assertEquals(application4.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(1).getName());
+        assertEquals(application3.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(2).getName());
+        assertEquals(application2.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(3).getName());
+        assertEquals(application1.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(4).getName());
+
+        uniqueApplicationList.sort(applicationStatusComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application5.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(4).getName());
+        assertEquals(application4.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(3).getName());
+        assertEquals(application3.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(2).getName());
+        assertEquals(application2.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(1).getName());
+        assertEquals(application1.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        // Check wildcard ordering - order by name
+        uniqueApplicationList.add(application6);
+        uniqueApplicationList.sort(applicationStatusComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application5.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+        assertEquals(application6.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(1).getName());
+
+        uniqueApplicationList.sort(applicationStatusComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application5.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(4).getName());
+        assertEquals(application6.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(5).getName());
+
+        // Applications without status tag
+        uniqueApplicationList.add(application7);
+        uniqueApplicationList.sort(applicationStatusComparator, ListCommand.COMMAND_ORDER_WORD_ASCENDING);
+        assertEquals(application7.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(0).getName());
+
+        uniqueApplicationList.sort(applicationStatusComparator, ListCommand.COMMAND_ORDER_WORD_DESCENDING);
+        assertEquals(application7.getName(), uniqueApplicationList.asUnmodifiableObservableList().get(6).getName());
+    }
+
 }
